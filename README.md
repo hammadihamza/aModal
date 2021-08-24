2982b23ccfc1dc3b2be4a944e11d4c758c961362
hammadihamza <hamzahammadi@protonmail.com>
2021-08-24 14:02:27 +0200

    Done nicely and differently

---
+++ README.md
@@ -0,0 +1,2 @@
+# FullPageScroll
+## is a page that contains other pages on which the full page scroll effect was implemented merely by css
---
+++ script.js
@@ -0,0 +1,7 @@
+var modalBtn = document.querySelector('.modal-btn');
+var modalBG = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
+
+modalClose.addEventListener('click',function(){
+    modalBG.style.display= 'none';
+})
